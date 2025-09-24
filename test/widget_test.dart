6a9e7fa8a9cd7a1c2722b4b9f5d361512786a2c5
annisa :b <annisaputriaprilia12@gmail.com>
2025-09-24 08:45:37 +0700

    assets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:animefirst/main.dart';
+import 'package:animeverse/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
